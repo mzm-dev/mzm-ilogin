4c2e0d418fb121a30476c9ed0b1853b99ef31610
Zaki Mustafa <mohdzaki04@gmail.com>
2024-10-18 14:58:17 +0800

    update

--- config/ilogin.php
+++ config/ilogin.php
@@ -2,8 +2,8 @@
 
 return [
     'enable' => env('SSO_ENABLE', true),
-    'api_url' => env('SSO_API_URL', 'https://sso.ns.test'),
-    'origin' => env('SSO_ORIGIN', 'espata.ns.test'),
-    'token' => env('SSO_TOKEN', 'c94ea6de-1423-4132-a57f-275aaf4f9185'),
+    'api_url' => env('SSO_API_URL', ''),
+    'origin' => env('SSO_ORIGIN', ''),
+    'token' => env('SSO_TOKEN', ''),
     'home' => 'home'
 ];
